d706c57cab85984626d1cabb46755e7821463347
kenny <kennyn2@uw.edu>
2020-02-28 13:37:42 -0800

    Add final changes to code and more comments

--- scripts/summary_information.R
+++ scripts/summary_information.R
@@ -1,31 +1,37 @@
 library("dplyr")
 library("stringr")
 # A function that takes in a dataset and returns a list of info about it:
+dataset <- df3
 get_summary_info <- function(dataset) {
   ret <- list()
   ret$game_titles <- nrow(dataset) # Number of game titles
   ret$oldest_date <- dataset %>% # Oldest date of game recorded in dataset
-    mutate(date = substr(released, 1, 4)) %>%
-    na.omit() %>%
-    filter(date == min(date)) %>%
-    pull(date)
+    mutate(date = as.Date(released)) %>%
+    summarize(min = min(date, na.rm = TRUE)) %>%
+    pull()
   ret$genres <- dataset %>% # Number of unique genres
     group_by(Genre) %>%
     summarize(count = n()) %>%
-    pull(Genre)
+    pull(Genre) %>% # You can end here to getindividual genres
+    length()
   ret$platforms <- dataset %>% # Number of unique platforms
     group_by(Platform) %>%
+    summarize(count = n()) %>% # You can end here to get individual platforms
+    pull(Platform) %>%
+    length()
+  ret$top_three_platforms <- dataset %>% # Top three platforms with most games
+    group_by(Platform) %>%
     summarize(count = n()) %>%
-    pull(Platform)
+    arrange(-count) %>%
+    top_n(3) %>%
+    pull(Platform) %>%
+    as.character()
   ret$publishers <- dataset %>% # Number of unique publishers
     group_by(Publisher) %>%
     summarize(count = n()) %>%
-    pull(Publisher)
-  ret$sales <- dataset %>% # Total global sales of games
-    select(Global_Sales) %>%
-    na.omit() %>%
-    summarize(total_sales = sum()) %>%
-    pull(total_sales)
+    pull(Publisher) %>% # You can end here to get individual publishers
+    length()
+  ret$sales <- paste(sum(na.omit(dataset$Global_Sales)), "million") # Total global sales of games
   # do some more interesting stuff
   return (ret)
 }
