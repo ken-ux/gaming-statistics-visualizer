ab32ff18fbe618d11e1b7d3beed51013d26a9163
kenny <kennyn2@uw.edu>
2020-02-28 02:27:30 -0800

    Add basic code for summary information

--- scripts/summary_information.R
+++ scripts/summary_information.R
@@ -1,13 +1,33 @@
 library("dplyr")
+library("stringr")
 # A function that takes in a dataset and returns a list of info about it:
 get_summary_info <- function(dataset) {
   ret <- list()
   ret$game_titles <- nrow(dataset) # Number of game titles
-  ret$oldest_date <- # Oldest date of game recorded in dataset
-  ret$genres <- # Number of unique genres
-  ret$platforms <- # Number of unique platforms
-  ret$publishers <- # Number of unique publishers
-  ret$sales <- # Total sales of games
+  ret$oldest_date <- df3 %>% # Oldest date of game recorded in dataset
+    mutate(date = substr(released, 1, 4)) %>%
+    desc() %>%
+    select(date) %>%
+    na.omit() %>%
+    top_n(1)
+  ret$genres <- df3 %>% # Number of unique genres
+    group_by(Genre) %>%
+    summarize(count = n()) %>%
+    pull(Genre)
+  ret$platforms <- df3 %>% # Number of unique platforms
+    group_by(Platform) %>%
+    summarize(count = n()) %>%
+    pull(Platform)
+  ret$publishers <- df3 %>% # Number of unique publishers
+    group_by(Publisher) %>%
+    summarize(count = n()) %>%
+    pull(Publisher)
+  ret$sales <- df3 %>% # Total global sales of games
+    select(Global_Sales) %>%
+    na.omit() %>%
+    group_by(Global_Sales) %>%
+    summarize(total_sales = sum()) %>%
+    pull(total_sales)
   # do some more interesting stuff
   return (ret)
 }
