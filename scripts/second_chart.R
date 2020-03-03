0538797e38e056353bd1ae92ca00a2f0d06013b5
Peter Miyoshi <peterkmiyoshi@gmail.com>
2020-03-02 21:28:09 -0800

    Added observations of chart 2

--- scripts/second_chart.R
+++ scripts/second_chart.R
@@ -4,16 +4,11 @@ library("stringr")
 library("ggplot2")
 library("leaflet")
 
-# Temp df for testing
-merged_df <- read.csv("data/merged_game_data.csv", stringsAsFactors = FALSE)
 get_second_chart <- function(df) {
-  ret <- ggplot(data = df, aes(x = Genre, y = Total_Shipped)) +
+  ggplot(data = df, aes(x = Genre, y = Total_Shipped)) +
     geom_boxplot(na.rm = TRUE) +
     ggtitle("Boxplot of Genre vs Total Shipped") +
     xlab("Genres") +
     ylab("Total Shipped (millions)") +
     theme(axis.text.x = element_text(angle=90, hjust=1))
-  ret
 }
-
-get_second_chart(merged_df)
