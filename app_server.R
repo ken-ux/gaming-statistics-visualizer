115f0ef5b0d913d9f7cfe03b48329f845c619713
kenny <kennyn2@uw.edu>
2020-03-09 16:57:49 -0700

    Create R file

---
+++ app_server.R
@@ -0,0 +1,32 @@
+# Keep track of any libraries you use and add it to the list in app.R
+my_server <- function(input, output) {
+  output$summary_text <- renderText({
+    "We are interested in gaming as a domain because it
+     explores a realm of technology that has gained traction
+     and popularity over time, producing complex and interesting
+     advancements in elements such as gaming platforms, companies,
+     and technological innovations (VR, mobile, etc.) Our
+     group has played different video games throughout our lives
+     and it would be interesting to see how our perspective of its
+     evolution parallels trends we uncover."
+    })
+
+
+  # Kenny
+
+
+
+
+  # Peter
+
+
+
+
+  # Ryan
+  
+  
+  
+  
+  
+  
+}
