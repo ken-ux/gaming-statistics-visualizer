fc28ca3f13cb37bae976fb36dacfd9f7767a1581
kenny <kennyn2@uw.edu>
2020-03-10 21:29:50 -0700

    Fix order of source files

--- app.R
+++ app.R
@@ -3,13 +3,14 @@ library("dplyr")
 library("shiny")
 library("ggplot2")
 library("stringr")
+library("ggrepel")
 
 # Import .csv as dataframe
 game_data <- read.csv("data/merged_game_data.csv", stringsAsFactors = FALSE)
 
 
 # Source UI and server from respective files
-source("app_ui.R")
 source("app_server.R")
+source("app_ui.R")
 
 shinyApp(ui = my_ui, server = my_server)
